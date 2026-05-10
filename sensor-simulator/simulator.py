99dca7efcc4f29f4c9d08b99585c06187978d939
elifduzcan <elifduzcan6@gmail.com>
2026-05-10 21:06:49 +0300

    mqtt simualator problems fixed

--- sensor-simulator/simulator.py
+++ sensor-simulator/simulator.py
@@ -13,6 +13,15 @@ SECRET_KEY = b"railway-digital-twin-secret"
 
 segments = ["S1", "S2", "S3", "S4", "S5", "S6"]
 
+SENSORS = {
+    "S1": 109,
+    "S2": 110,
+    "S3": 111,
+    "S4": 112,
+    "S5": 113,
+    "S6": 114
+}
+
 client = mqtt.Client()
 client.connect(BROKER, PORT, 60)
 
@@ -32,24 +41,27 @@ def calculate_digital_signature(crc_hash):
 
 def generate_sensor_data(segment_id):
     temperature = round(random.uniform(25, 38), 2)
-    vibration = round(random.uniform(0.5, 2.2), 2)
+    vibration_x = round(random.uniform(0.5, 2.2), 2)
+    vibration_y = round(random.uniform(0.3, 1.8), 2)
+    vibration_z = round(random.uniform(0.3, 1.8), 2)
     tilt = round(random.uniform(-1.5, 1.5), 2)
 
-    # Simulated anomalies according to the project scenario
     if segment_id == "S5" and random.random() < 0.25:
         temperature = round(random.uniform(41, 48), 2)
 
     if segment_id == "S3" and random.random() < 0.20:
-        vibration = round(random.uniform(2.8, 4.0), 2)
+        vibration_x = round(random.uniform(2.8, 4.0), 2)
 
     payload = {
-        "sensorId": f"RAY-{segment_id}-001",
+        "sensorId": SENSORS[segment_id],
         "segmentId": segment_id,
         "sensorType": "RAY_SENSOR",
         "timestamp": datetime.now().isoformat(timespec="seconds"),
         "samplingFrequency": 10,
         "temperature": temperature,
-        "vibration": vibration,
+        "vibrationX": vibration_x,
+        "vibrationY": vibration_y,
+        "vibrationZ": vibration_z,
         "tilt": tilt
     }
 
