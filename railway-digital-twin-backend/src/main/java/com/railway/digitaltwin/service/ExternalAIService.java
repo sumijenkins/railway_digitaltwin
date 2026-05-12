af574e67fad2390ecbf90194541155e09bd9cdba
elifduzcan <elifduzcan6@gmail.com>
2026-05-12 14:07:49 +0300

    externalaiservice url changed

--- railway-digital-twin-backend/src/main/java/com/railway/digitaltwin/service/ExternalAIService.java
+++ railway-digital-twin-backend/src/main/java/com/railway/digitaltwin/service/ExternalAIService.java
@@ -27,7 +27,7 @@ public class ExternalAIService {
     }
     public Map<String, Object> predictRul(SensorFeature feature) {
 
-        String url = "http://localhost:5000/rul";
+        String url = "http://ai-service:5000/rul";
 
         Map<String, Object> request = new HashMap<>();
         request.put("rms", feature.getRms());
@@ -40,7 +40,7 @@ public class ExternalAIService {
     }
     public Map<String, Object> explainPrediction(SensorFeature feature) {
 
-        String url = "http://localhost:5000/xai";
+        String url = "http://ai-service:5000/xai";
 
         Map<String, Object> request = new HashMap<>();
         request.put("rms", feature.getRms());
